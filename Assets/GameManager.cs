7435529b3ae7c0ae6e6d0de23172623dd2c7c8fa
k2sebeom <moses97@gmail.com>
2021-11-01 20:04:10 +0900

    feat: Add many things

--- Assets/GameManager.cs
+++ Assets/GameManager.cs
@@ -1,25 +1,51 @@
 ﻿using System.Collections;
 using System.Collections.Generic;
 using UnityEngine;
+using UnityEngine.UI;
 
 public class GameManager : MonoBehaviour
 {
     public GameObject startButton;
+    public Image gameOverLogo;
+    public Text timerText;
+    public DollController dollController;
+
+    private float timeLeft = 60;
 
     public void StartGame()
     {
         startButton.SetActive(false);
+        timeLeft = 10;
+        StartCoroutine("Tick");
     }
 
-    // Start is called before the first frame update
-    void Start()
+    public void EndGame()
     {
-        
+        timeLeft = 0;
+        dollController.StopAllCoroutines();
+        dollController.gameObject.SetActive(false);
+        StartCoroutine("FadeIn");
     }
 
-    // Update is called once per frame
-    void Update()
+    IEnumerator FadeIn()
     {
-        
+        gameOverLogo.gameObject.SetActive(true);
+        while(gameOverLogo.color.a < 1)
+        {
+            gameOverLogo.color += new Color(0, 0, 0, 0.1f);
+            yield return new WaitForSeconds(0.1f);
+        }
     }
+
+    IEnumerator Tick()
+    {
+        while (timeLeft > 0)
+        {
+            yield return new WaitForSeconds(1);
+            timeLeft -= 1;
+            timerText.text = timeLeft.ToString();
+        }
+        EndGame();
+    }
+
 }
